3dec9ca65b9715ac51f407c2c6a1484dc7dc5eca
Christopher McDevitt <christophermcdevitt@gmail.com>
2015-10-22 13:20:06 +0100

    feat: use turtle to run a list of install commands

--- src/Lib.hs
+++ src/Lib.hs
@@ -7,11 +7,14 @@ module Lib
 
 import           Control.Monad         (mzero)
 import qualified Data.ByteString.Char8 as BS
+import qualified Data.Text             as Text
 import           Data.Yaml             ((.:), (.:?))
 import qualified Data.Yaml             as Yaml
+import           Turtle                (shell, (<>))
+import qualified Turtle                as T
 
 data Build = Build { language :: String
-                   , install  :: [String]
+                   , install  :: [T.Text]
                    , blurb    :: Maybe String}
              deriving (Show)
 
@@ -24,19 +27,23 @@ instance Yaml.FromJSON Build where
     parseJSON _ = mzero
 
 
-strErr :: Show a => a  -> String
-strErr s = "Error: " ++ (show s)
-
 getYaml :: FilePath -> IO BS.ByteString
 getYaml = BS.readFile
 
-moo :: Build -> String
-moo buildDesc = show buildDesc --(map (\r -> (language r, install r)) buildDesc)
+runCmd cmd = do
+  res <- shell cmd T.empty
+  case res of
+    T.ExitSuccess -> return ()
+    T.ExitFailure n -> T.die ("Shell cmd: " <> cmd <> " failed with code: " <> T.repr n)
+
+runCmds cmds = T.sh $  do
+  res <- T.select cmds
+  T.liftIO (runCmd res)
 
 someFunc :: IO ()
 someFunc =
     do
-      d <- (Yaml.decodeEither <$> (getYaml "build.yml"))
+      d <- Yaml.decodeEither <$> getYaml "build.yml"
       case d of
-        Left err -> print $ strErr err
-        Right ps -> print $ moo ps
+        Left err -> T.die $ Text.pack err
+        Right ps -> runCmds (install ps)
